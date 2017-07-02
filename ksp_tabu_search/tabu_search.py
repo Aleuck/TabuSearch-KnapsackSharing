74cae46c17db674ae21dbcb24d03f98241ea84a0
Alexandre Leuck <aleuck@inf.ufrgs.br>
2017-07-02 18:24:59 -0300

    knapsacksharing python model

--- ksp_tabu_search/tabu_search.py
+++ ksp_tabu_search/tabu_search.py
@@ -1,36 +1,61 @@
-import sys, getopt
+import sys, getopt, random, numpy
+
+class KnapsackSharing:
+    capacity = 0
+    numItens = 0
+    numGroups = 0
+    itens = None
+    def __init__(self, capacity, num_itens, num_groups):
+        self.capacity = capacity
+        self.numItens = num_itens
+        self.numGroups = num_groups
+        self.itens = numpy.zeros(shape=[self.numItens,3],dtype=numpy.int16)
+    def fitness(solution):
+        group_profit = numpy.zeros(self.numGroups, dtype=numpy.int32)
+        total_weigth = 0
+        for index, value in enumerate(solution):
+            if value == 1:
+                group_profit[self.itens[index,2]] += self.itens[index,1]
+                total_weigth += self.itens[index,0]
+        feasable = total_weigth <= self.capacity
+        return feasable, group_profit.min()
 
 def main(argv):
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"s:",["seed="])
     except getopt.GetoptError:
-        print ("Error(command should be: instances_to_glpk.py -i <inputfile> -o <outputfile>)")
+        print ("Error(command should be: %s -s <seed>)" % sys.argv[0])
         sys.exit(2)
     for opt, arg in opts:
-        if opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
+        if opt in ("-s", "--seed"):
+            seed = int(arg)
+    try:
+        seed
+    except NameError:
+        seed = random.randint(1, sys.maxsize)
+    print("seed = %s" % seed)
+    random.seed(seed)
 
-    with open(inputfile) as f:
-        n = int(f.readline())
-        g = int(f.readline())
-        c = int(f.readline())
-        
-        tam_grupos = map(int,f.readline().split())
-        groups = []
-        for tam in tam_grupos:
-            group = []
-            for j in range(tam):
-                s,p = f.readline().split()
-                s,p = int(s), int(p);
-                group.append([s,p])
-            groups.append(group)
+    numItens = int(input())
+    numGroups = int(input())
+    knapsackCapacity = int(input())
+    sizeGroups = list(map(int,input().split()))
+    print ("n=%d, g=%d, c=%d" % (numItens,numGroups,knapsackCapacity))
+    for i, size in enumerate(sizeGroups):
+        print ("group %d has %d elements" % (i, size))
+    currentGroup = 0
+    groupBase = 0
+    ksp = KnapsackSharing(capacity=knapsackCapacity, num_itens=numItens, num_groups=numGroups)
+    for i, line in enumerate(sys.stdin):
+        if i - groupBase >= sizeGroups[currentGroup]:
+            currentGroup += 1;
+            groupBase = i;
+        aux = list(map(int,line.split()))
+        aux.append(currentGroup)
+        ksp.itens[i] = aux
 
-    with open(outputfile, "w") as of:
-        
 
 if __name__ == "__main__":
    main(sys.argv[1:])
